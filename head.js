198b33e11d7be716072cae9273bf84d0da3dd7a1
Dickson Yu <dickson.yu@email.com>
2021-11-22 16:08:29 -0500

    Adding array head

---
+++ head.js
@@ -0,0 +1,18 @@
+// Function Implementation
+// Function to assert equal
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(actual) {
+  return actual[0];
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), 5);
