1b88ff82ffc406e2e5cf449ef15355a32e9a8930
Dickson Yu <dickson.yu@email.com>
2021-11-29 15:23:38 -0500

    Refactored tail and middle to use mocha&chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,13 @@
-const assertEqual = require(`../assertEqual`)
+const assert = require(`chai`).assert;
 const tail = require(`../tail`)
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result,["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it ("returns 'Lighthouse', 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+});
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
+// // TEST CODE
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result,["Lighthouse", "Labs"]);
