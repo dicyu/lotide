24d463ef17857c7700d0e54981ab362a886534e8
Dickson Yu <dickson.yu@email.com>
2021-11-29 13:15:14 -0500

    modularizing eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-// Function Implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
   // function to compare arrays
   const eqArrays = function(arr1, arr2) {
     if (arr1.length !== arr2.length)
@@ -18,8 +10,4 @@ const assertEqual = function(actual, expected) {
     return true;
   }
 
-// Code Test
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  module.exports = eqArrays;
