ee5ca05d2fa3d102bc427e82cf5ee704c97da2a8
Dickson Yu <dickson.yu@email.com>
2021-11-29 10:13:56 -0500

    modularizing tail and refactored

--- tail.js
+++ tail.js
@@ -1,22 +1,5 @@
-// Function Implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const tail = function(actual) {
   return actual.slice(1);
 };
 
-
-
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result,["Lighthouse", "Labs"]);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
