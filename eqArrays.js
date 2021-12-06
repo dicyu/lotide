849ad15fec13780bf2949814dd15b46f42e9cda2
Dickson Yu <dickson.yu@email.com>
2021-12-05 21:42:23 -0500

    added recursive

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,18 @@
-  // function to compare arrays
-  const eqArrays = function(arr1, arr2) {
-    if (arr1.length !== arr2.length)
-      return false;
-    for (let x = 0; x < arr1.length; x++) {
-      if (arr1[x] !== arr2[x]) {
+// function to compare arrays
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+
+  for (let x = 0; x < arr1.length; x++) {
+    if (Array.isArray(arr1[x]) && Array.isArray(arr2[x])) {
+      if (!eqArrays(arr1[x], arr2[x])) {
         return false;
       }
+    } else if (arr1[x] !== arr2[x]) {
+      return false;
     }
-    return true;
   }
+  return true;
+};
 
-  module.exports = eqArrays;
+module.exports = eqArrays;
