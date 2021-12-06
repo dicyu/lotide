1ef7d1c0f8abdc447473262ee44625548b146811
Dickson Yu <dickson.yu@email.com>
2021-12-05 20:59:54 -0500

    added recursion

--- eqObjects.js
+++ eqObjects.js
@@ -1,26 +1,5 @@
-// Function to see if both sides equal to each other or fail
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require(`./eqArrays`);
 
-// function to compare arrays
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length)
-    return false;
-  for (let x = 0; x < arr1.length; x++) {
-    if (arr1[x] !== arr2[x]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// Returns true if both objects have identical keys with identical values
-// Otherwise you get back a fat false!
 const eqObjects = function(object1, object2) {
   const objArr1 = Object.keys(object1);
   const objArr2 = Object.keys(object2);
@@ -37,28 +16,8 @@ const eqObjects = function(object1, object2) {
         }
       }
     }
-    return true;
   }
+  return true;
 };
 
-
-// Test Code
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const abc = { a: "1", b: "2", c: "3" };
-
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-const cd2 = { c: "1", d: ["2", 3, 4] };
-
-assertEqual(eqObjects(ab, ba), true); // => true
-assertEqual(eqObjects(ab, abc), false); // => false
-
-assertEqual(eqObjects(cd, dc), true); // => true
-assertEqual(eqObjects(cd, cd2), false); // => false
-
-const arr1 = { a: [1, 2], b: 1 };
-const arr2 = { a: [1, 2], b: 1 };
-
-assertEqual(eqObjects(arr1, arr2), true);
+module.exports = eqObjects;
